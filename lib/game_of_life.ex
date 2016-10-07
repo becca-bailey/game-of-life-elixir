ee7a332f6dba7c6a6da5d47bc3aabe06bb00b065
Becca Nelson <beccanelson88@gmail.com>
2016-10-07 15:56:06 -0500

    Counts horizontal neighbors

--- lib/game_of_life.ex
+++ lib/game_of_life.ex
@@ -38,8 +38,8 @@ defmodule GameOfLife do
     List.update_at(world, index(x), &(List.replace_at(&1, index(y), :alive)))
   end
 
-  def is_alive_at(world, {x, y}) do
-    Enum.at(world, index(x)) |> Enum.at(index(y)) |> is_alive 
+  def alive_at?(world, {x, y}) do
+    get_at_coordinate(world, x) |> get_at_coordinate(y) |> alive? 
   end
 
   def tick(world) when world == [] do
@@ -51,15 +51,50 @@ defmodule GameOfLife do
     |> kill_at({1, 2})
   end
 
+  def neighbors_count(world, {x, y}) do
+    coordinates_to_check = 
+      get_horizontal_coordinates(world, {x, y})
+    neighbors = Enum.map(coordinates_to_check, &(get_value_from_coordinate_pair(world, &1)))
+    Enum.count(neighbors, &(&1 == :alive))
+  end
+
+  defp get_horizontal_coordinates(world, {x, y}) do
+    cond do
+      length(world) == 1 ->
+        []
+      x == 1 ->  
+        [{2, y}]
+      x == length(world)
+        [{length(world) - 1, y}]
+      true ->
+        [{x - 1, y}, {x + 1, y}]
+    end
+  end
+
+  defp get_value_from_coordinate_pair(coll, {x, y}) do
+    coll 
+    |> get_at_coordinate(x)
+    |> get_at_coordinate(y)
+  end
+
+
+  defp get_at_coordinate(coll, coordinate) do
+    Enum.at(coll, index(coordinate))
+  end
+
   defp index(n) do
     n - 1
+  end
+
+  defp coordinate(n) do
+    n + 1
   end
 
   defp kill_at(world, {x, y}) do
     List.update_at(world, index(x), &(List.replace_at(&1, index(y), :dead)))
   end
 
-  defp is_alive(cell) do
+  defp alive?(cell) do
     cell == :alive
   end
   
--- test/game_of_life_test.exs
+++ test/game_of_life_test.exs
@@ -1,52 +1,69 @@
 defmodule GameOfLifeTest do
   use ExUnit.Case
   doctest GameOfLife
+  alias GameOfLife, as: Game
   
   test "a new game has an empty world" do
-    assert GameOfLife.new |> GameOfLife.is_empty 
+    assert Game.new |> Game.is_empty 
   end
 
   test "a living cell can be added to the world" do
-    assert GameOfLife.new 
-      |> GameOfLife.world_size(1)
-      |> GameOfLife.set_living_at({1, 1}) 
-      |> GameOfLife.is_alive_at({1, 1})
+    assert Game.new 
+      |> Game.world_size(1)
+      |> Game.set_living_at({1, 1}) 
+      |> Game.alive_at?({1, 1})
   end
 
   test "after adding a cell the world is not empty" do
-    refute GameOfLife.new 
-      |> GameOfLife.world_size(1)
-      |> GameOfLife.set_living_at({1, 1})
-      |> GameOfLife.is_empty
+    refute Game.new 
+      |> Game.world_size(1)
+      |> Game.set_living_at({1, 1})
+      |> Game.is_empty
   end
 
   test "an empty world is still empty after one tick" do
-    assert GameOfLife.new 
-      |> GameOfLife.tick 
-      |> GameOfLife.is_empty
+    assert Game.new 
+      |> Game.tick 
+      |> Game.is_empty
   end
 
   test "a living cell with no neighbors dies in the next generation" do
-    refute GameOfLife.new
-      |> GameOfLife.world_size(1)
-      |> GameOfLife.set_living_at({1, 1})
-      |> GameOfLife.tick
-      |> GameOfLife.is_alive_at({1, 1}) 
+    refute Game.new
+      |> Game.world_size(1)
+      |> Game.set_living_at({1, 1})
+      |> Game.tick
+      |> Game.alive_at?({1, 1}) 
   end
 
   test "a living cell with only one neighbor dies in the next generation" do
-    refute GameOfLife.new
-      |> GameOfLife.world_size(2)
-      |> GameOfLife.set_living_at({1, 1})
-      |> GameOfLife.set_living_at({1, 2})
-      |> GameOfLife.tick
-      |> GameOfLife.is_alive_at({1, 1})
+    refute Game.new
+      |> Game.world_size(2)
+      |> Game.set_living_at({1, 1})
+      |> Game.set_living_at({1, 2})
+      |> Game.tick
+      |> Game.alive_at?({1, 1})
     
-    refute GameOfLife.new
-      |> GameOfLife.world_size(2)
-      |> GameOfLife.set_living_at({1, 1})
-      |> GameOfLife.set_living_at({1, 2})
-      |> GameOfLife.tick
-      |> GameOfLife.is_alive_at({1, 2}) 
+    refute Game.new
+      |> Game.world_size(2)
+      |> Game.set_living_at({1, 1})
+      |> Game.set_living_at({1, 2})
+      |> Game.tick
+      |> Game.alive_at?({1, 2}) 
+  end
+
+  test "a living single cell has no neighbors" do
+    world = Game.new
+           |> Game.world_size(1)
+           |> Game.set_living_at({1, 1})
+    assert Game.neighbors_count(world, {1, 1}) == 0
+  end
+
+  test "a living cell next to another living cell has one neighbor" do
+    world = Game.new
+            |> Game.world_size(2)
+            |> Game.set_living_at({1, 1})
+            |> Game.set_living_at({1, 2})
+    assert Game.neighbors_count(world, {1, 1}) == 1
+    assert Game.neighbors_count(world, {1, 2}) == 1
   end
 end
